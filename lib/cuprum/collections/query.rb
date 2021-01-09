4a4ac09679941cf32b5d9ff7f9b2fd8dce362688
Rob Smith <merlin@sleepingkingstudios.com>
2021-01-09 01:12:28 -0500

    Implement Cuprum::Collections::Query.

---
+++ lib/cuprum/collections/query.rb
@@ -0,0 +1,238 @@
+# frozen_string_literal: true
+
+require 'cuprum/collections'
+
+module Cuprum::Collections
+  # Abstract base class for collection Query implementations.
+  class Query
+    ORDER_HASH_VALUES = {
+      asc:        :asc,
+      ascending:  :asc,
+      desc:       :desc,
+      descending: :desc
+    }.freeze
+    private_constant :ORDER_HASH_VALUES
+
+    def initialize
+      @criteria = []
+    end
+
+    # Returns a normalized representation of the query criteria.
+    #
+    # The query criteria define which data from the collection matches the
+    # query. Specifically, an item in the collection matches the query if and
+    # only if it matches each criterion. If the query has no criteria, then it
+    # will match all items in the collection.
+    #
+    # Each criterion is represented as an Array with three elements:
+    # - The name of the property or column to select by.
+    # - The operation to filter, such as :eq (an equality operation).
+    # - The expected value.
+    #
+    # For example, a query that selects all items whose :series property is
+    # equal to 'The Lord of the Rings' would have the following criterion:
+    # `[:series, :eq, 'The Lord of the Rings']`.
+    #
+    # @return [Array<Array>] the query criteria.
+    #
+    # @see #where
+    def criteria
+      @criteria.dup
+    end
+
+    # Returns a copy of the query with the specified limit.
+    #
+    # The query will return at most the specified number of items.
+    #
+    # When #limit is called on a query that already defines a limit, the old
+    # limit is replaced with the new.
+    #
+    # @param count [Integer] the maximum number of items to return.
+    #
+    # @return [Query] the copy of the query.
+    def limit(count)
+      validate_limit(count)
+
+      dup.tap { |copy| copy.with_limit(count) }
+    end
+
+    # Returns a copy of the query with the specified offset.
+    #
+    # The query will skip the specified number of matching items, and return
+    # only matching items after the given offset. If the total number of
+    # matching items is less than or equal to the offset, the query will not
+    # return any items.
+    #
+    # When #offset is called on a query that already defines an offset, the old
+    # offset is replaced with the new.
+    #
+    # @param count [Integer] the number of items to skip.
+    #
+    # @return [Query] the copy of the query.
+    def offset(count)
+      validate_offset(count)
+
+      dup.tap { |copy| copy.with_offset(count) }
+    end
+
+    # Returns a copy of the query with the specified order.
+    #
+    # The query will find the matching items, sort them in the specified order,
+    # and then apply limit and/or offset (if applicable) to determine the final
+    # returned items.
+    #
+    # When #order is called on a query that already defines an ordering, the old
+    # ordering is replaced with the new.
+    #
+    # @return [Query] the copy of the query.
+    #
+    # @example Sorting By Attribute Names
+    #   # This query will sort books by author (ascending), then by title
+    #   # (ascending) within authors.
+    #   query = query.order(:author, :title)
+    #
+    # @example Sorting With Directions
+    #   # This query will sort books by series (ascending), then by the date of
+    #   # publication (descending) within series.
+    #   query = query.order({ series: :asc, published_at: :desc })
+    #
+    # @overload order(*attributes)
+    #   Orders the results by the given attributes, ascending, and in the
+    #   specified order, i.e. items with the same value of the first attribute
+    #   will be sorted by the second (if any), and so on.
+    #
+    #   @param attributes [Array<String, Symbol>] The attributes to order by.
+    #
+    # @overload order(attributes)
+    #   Orders the results by the given attributes and sort directions, and in
+    #   the specified order.
+    #
+    #   @param attributes [Hash{String,Symbol=>Symbol}] The attributes to order
+    #     by. The hash keys should be the names of attributes or columns, and
+    #     the corresponding values should be the sort direction for that
+    #     attribute, either :asc or :desc.
+    def order(first, *rest)
+      normalized = normalize_order(first, *rest)
+
+      dup.tap { |copy| copy.with_order(normalized) }
+    end
+    alias order_by order
+
+    # Returns a copy of the query with the specified filters.
+    #
+    # The given parameters are used to construct query criteria, which define
+    # which data from the collection matches the query. Specifically, an item in
+    # the collection matches the query if and only if it matches each criterion.
+    # If the query has no criteria, then it will match all items in the
+    # collection.
+    #
+    # When #where is called on a query that already defines criteria, then the
+    # new criteria are appended to the old. Any items in the collection must
+    # match both the old and the new criteria to be returned by the query.
+    #
+    # @example Filtering Data By Equality
+    #   # The query will only return items whose author is 'J.R.R. Tolkien'.
+    #   query = query.where { { author: 'J.R.R. Tolkien' } }
+    #
+    # @example Filtering Data By Operator
+    #   # The query will only return items whose author is 'J.R.R. Tolkien',
+    #   # and whose series is not 'The Lord of the Rings'.
+    #   query = query.where do
+    #     {
+    #       author: eq('J.R.R. Tolkien'),
+    #       series: ne('The Lord of the Rings')
+    #     }
+    #   end
+    #
+    # @overload where(&block)
+    #   @yield The given block is passed to a QueryBuilder, which converts the
+    #     block to query criteria and generates a new query using those
+    #     criteria.
+    #
+    #   @yieldreturn [Hash] The filters to apply to the query. The hash keys
+    #     should be the names of attributes or columns, and the corresponding
+    #     values should be either the literal value for that attribute or a
+    #     method call for a valid operation defined for the query.
+    #
+    # @see #criteria
+    def where(&block)
+      query_builder.call(&block)
+    end
+
+    protected
+
+    def with_criteria(criteria)
+      @criteria += criteria
+
+      self
+    end
+
+    private
+
+    def invalid_order_error
+      'order must be a list of attribute names and/or a hash of attribute' \
+      ' names with values :asc or :desc'
+    end
+
+    def normalize_order(*attributes)
+      qualified = attributes.last.is_a?(Hash) ? attributes.pop : {}
+      qualified = normalize_order_hash(qualified)
+
+      validate_order_attributes(attributes)
+
+      attributes
+        .each
+        .with_object({}) { |attribute, hsh| hsh[attribute.intern] = :asc }
+        .merge(qualified)
+        .tap { |hsh| validate_order_normalized(hsh) }
+    end
+
+    def normalize_order_hash(hsh)
+      hsh.each.with_object({}) do |(key, value), normalized|
+        unless valid_order_hash_key?(key)
+          raise ArgumentError, invalid_order_error, caller(2..-1)
+        end
+
+        normalized[key.intern] = normalize_order_hash_value(value)
+      end
+    end
+
+    def normalize_order_hash_value(value)
+      value = value.downcase if value.is_a?(String)
+
+      ORDER_HASH_VALUES.fetch(value.is_a?(String) ? value.intern : value) do
+        raise ArgumentError, invalid_order_error, caller(3..-1)
+      end
+    end
+
+    def valid_order_hash_key?(key)
+      (key.is_a?(String) || key.is_a?(Symbol)) && !key.to_s.empty?
+    end
+
+    def validate_limit(count)
+      return if count.is_a?(Integer) && !count.negative?
+
+      raise ArgumentError, 'limit must be a non-negative integer', caller(1..-1)
+    end
+
+    def validate_offset(count)
+      return if count.is_a?(Integer) && !count.negative?
+
+      raise ArgumentError,
+        'offset must be a non-negative integer',
+        caller(1..-1)
+    end
+
+    def validate_order_attributes(attributes)
+      return if attributes.all? do |item|
+        (item.is_a?(String) || item.is_a?(Symbol)) && !item.to_s.empty?
+      end
+
+      raise ArgumentError, invalid_order_error, caller(2..-1)
+    end
+
+    def validate_order_normalized(hsh)
+      raise ArgumentError, invalid_order_error, caller(2..-1) if hsh.empty?
+    end
+  end
+end
---
+++ spec/cuprum/collections/query_spec.rb
@@ -0,0 +1,627 @@
+# frozen_string_literal: true
+
+require 'cuprum/collections/query'
+
+RSpec.describe Cuprum::Collections::Query do
+  subject(:query) { described_class.new }
+
+  let(:described_class) { Spec::ExampleQuery }
+
+  example_class 'Spec::ExampleQuery', Cuprum::Collections::Query do |klass| # rubocop:disable RSpec/DescribedClass
+    klass.define_method(:query_builder) {}
+    klass.define_method(:with_limit)    { |_count| }
+    klass.define_method(:with_offset)   { |_count| }
+    klass.define_method(:with_order)    { |*_args| }
+  end
+
+  describe '.new' do
+    it { expect(described_class).to respond_to(:new).with(0).arguments }
+  end
+
+  describe '#criteria' do
+    include_examples 'should define reader', :criteria, []
+
+    it 'should not change the query criteria' do
+      expect { query.criteria << ['censored', :eq, true] }
+        .not_to change(query, :criteria)
+    end
+  end
+
+  describe '#limit' do
+    let(:copy) { described_class.new }
+
+    before(:example) do
+      allow(query).to receive(:dup).and_return(copy) # rubocop:disable RSpec/SubjectStub
+
+      allow(copy).to receive(:with_limit)
+    end
+
+    it { expect(query).to respond_to(:limit).with(1).argument }
+
+    it { expect(query.limit 3).to be copy }
+
+    it 'should delegate to #with_limit' do
+      query.limit 3
+
+      expect(copy).to have_received(:with_limit).with(3)
+    end
+
+    describe 'with nil' do
+      let(:error_message) { 'limit must be a non-negative integer' }
+
+      it 'should raise an exception' do
+        expect { query.limit nil }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an object' do
+      let(:error_message) { 'limit must be a non-negative integer' }
+
+      it 'should raise an exception' do
+        expect { query.limit Object.new.freeze }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a negative integer' do
+      let(:error_message) { 'limit must be a non-negative integer' }
+
+      it 'should raise an exception' do
+        expect { query.limit(-1) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+  end
+
+  describe '#offset' do
+    let(:copy) { described_class.new }
+
+    before(:example) do
+      allow(query).to receive(:dup).and_return(copy) # rubocop:disable RSpec/SubjectStub
+
+      allow(copy).to receive(:with_offset)
+    end
+
+    it { expect(query).to respond_to(:offset).with(1).argument }
+
+    it { expect(query.offset 3).to be copy }
+
+    it 'should delegate to #with_offset' do
+      query.offset 3
+
+      expect(copy).to have_received(:with_offset).with(3)
+    end
+
+    describe 'with nil' do
+      let(:error_message) { 'offset must be a non-negative integer' }
+
+      it 'should raise an exception' do
+        expect { query.offset nil }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an object' do
+      let(:error_message) { 'offset must be a non-negative integer' }
+
+      it 'should raise an exception' do
+        expect { query.offset Object.new.freeze }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a negative integer' do
+      let(:error_message) { 'offset must be a non-negative integer' }
+
+      it 'should raise an exception' do
+        expect { query.offset(-1) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+  end
+
+  describe '#order' do
+    let(:copy) { described_class.new }
+
+    before(:example) do
+      allow(query).to receive(:dup).and_return(copy) # rubocop:disable RSpec/SubjectStub
+
+      allow(copy).to receive(:with_order)
+    end
+
+    it 'should define the method' do
+      expect(query)
+        .to respond_to(:order)
+        .with(1).argument
+        .and_unlimited_arguments
+    end
+
+    it { expect(query).to alias_method(:order).as(:order_by) }
+
+    describe 'with nil' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order nil }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an object' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order Object.new.freeze }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an empty string' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order '' }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an empty symbol' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order :'' }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an empty hash' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order({}) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a hash with invalid keys' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order({ nil => :asc }) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a hash with empty string keys' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order({ '' => :asc }) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a hash with empty symbol keys' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order({ '': :asc }) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a hash with nil value' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order({ title: nil }) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a hash with object value' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order({ title: Object.new.freeze }) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a hash with empty value' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order({ title: '' }) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a hash with invalid value' do
+      let(:error_message) do
+        'order must be a list of attribute names and/or a hash of attribute' \
+        ' names with values :asc or :desc'
+      end
+
+      it 'should raise an exception' do
+        expect { query.order({ title: 'wibbly' }) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an attribute name as a string' do
+      let(:attribute) { 'title' }
+      let(:expected)  { { title: :asc } }
+
+      it { expect(query.order attribute).to be copy }
+
+      it 'should delegate to #with_offset' do
+        query.order(attribute)
+
+        expect(copy).to have_received(:with_order).with(expected)
+      end
+    end
+
+    describe 'with an attribute name as a symbol' do
+      let(:attribute) { :title }
+      let(:expected)  { { title: :asc } }
+
+      it { expect(query.order attribute).to be copy }
+
+      it 'should delegate to #with_offset' do
+        query.order(attribute)
+
+        expect(copy).to have_received(:with_order).with(expected)
+      end
+    end
+
+    describe 'with a list of attribute names as a string' do
+      let(:attributes) { %w[title author series] }
+      let(:expected)   { { title: :asc, author: :asc, series: :asc } }
+
+      it { expect(query.order(*attributes)).to be copy }
+
+      it 'should delegate to #with_offset' do
+        query.order(*attributes)
+
+        expect(copy).to have_received(:with_order).with(expected)
+      end
+    end
+
+    describe 'with a list of attribute names as a symbol' do
+      let(:attributes) { %i[title author series] }
+      let(:expected)   { { title: :asc, author: :asc, series: :asc } }
+
+      it { expect(query.order(*attributes)).to be copy }
+
+      it 'should delegate to #with_offset' do
+        query.order(*attributes)
+
+        expect(copy).to have_received(:with_order).with(expected)
+      end
+    end
+
+    describe 'with a hash with a string key' do
+      let(:key)      { :asc }
+      let(:hash)     { { 'title' => key } }
+      let(:expected) { { title: :asc } }
+
+      it { expect(query.order hash).to be copy }
+
+      describe 'with key: :asc' do
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: "asc"' do
+        let(:key) { 'asc' }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: :ascending' do
+        let(:key) { :ascending }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: "ascending"' do
+        let(:key) { 'ascending' }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: :desc' do
+        let(:key)      { :desc }
+        let(:expected) { { title: :desc } }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: "desc"' do
+        let(:key)      { 'desc' }
+        let(:expected) { { title: :desc } }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: :descending' do
+        let(:key)      { :descending }
+        let(:expected) { { title: :desc } }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: "descending"' do
+        let(:key)      { 'descending' }
+        let(:expected) { { title: :desc } }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+    end
+
+    describe 'with a hash with a symbol key' do
+      let(:key)      { :asc }
+      let(:hash)     { { title: key } }
+      let(:expected) { { title: :asc } }
+
+      it { expect(query.order hash).to be copy }
+
+      describe 'with key: :asc' do
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: "asc"' do
+        let(:key) { 'asc' }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: :ascending' do
+        let(:key) { :ascending }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: "ascending"' do
+        let(:key) { 'ascending' }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: :desc' do
+        let(:key)      { :desc }
+        let(:expected) { { title: :desc } }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: "desc"' do
+        let(:key)      { 'desc' }
+        let(:expected) { { title: :desc } }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: :descending' do
+        let(:key)      { :descending }
+        let(:expected) { { title: :desc } }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+
+      describe 'with key: "descending"' do
+        let(:key)      { 'descending' }
+        let(:expected) { { title: :desc } }
+
+        it 'should delegate to #with_offset' do
+          query.order(hash)
+
+          expect(copy).to have_received(:with_order).with(expected)
+        end
+      end
+    end
+
+    describe 'with a hash with string keys' do
+      let(:hash) do
+        { 'title' => :asc, 'author' => :desc, 'series' => :asc }
+      end
+      let(:expected) { { title: :asc, author: :desc, series: :asc } }
+
+      it { expect(query.order hash).to be copy }
+
+      it 'should delegate to #with_offset' do
+        query.order(hash)
+
+        expect(copy).to have_received(:with_order).with(expected)
+      end
+    end
+
+    describe 'with a hash with symbol keys' do
+      let(:hash)     { { title: :asc, author: :desc, series: :asc } }
+      let(:expected) { { title: :asc, author: :desc, series: :asc } }
+
+      it { expect(query.order hash).to be copy }
+
+      it 'should delegate to #with_offset' do
+        query.order(hash)
+
+        expect(copy).to have_received(:with_order).with(expected)
+      end
+    end
+
+    describe 'with a list of attribute names and a hash' do
+      let(:attributes) { %i[publisher page_count] }
+      let(:hash)       { { title: :asc, author: :desc, series: :asc } }
+      let(:expected) do
+        {
+          publisher:  :asc,
+          page_count: :asc,
+          title:      :asc,
+          author:     :desc,
+          series:     :asc
+        }
+      end
+
+      it { expect(query.order hash).to be copy }
+
+      it 'should delegate to #with_offset' do
+        query.order(*attributes, hash)
+
+        expect(copy).to have_received(:with_order).with(expected)
+      end
+    end
+  end
+
+  describe '#where' do
+    let(:block)   { -> { { title: 'The Caves of Steel' } } }
+    let(:other)   { described_class.new }
+    let(:builder) { -> { other } }
+
+    before(:example) do
+      allow(query).to receive(:query_builder).and_return(builder) # rubocop:disable RSpec/SubjectStub
+    end
+
+    it { expect(query).to respond_to(:where).with(0).arguments.and_a_block }
+
+    it { expect(query.where(&block)).to be other }
+
+    it 'should delegate to the query builder' do
+      allow(builder).to receive(:call).and_yield
+
+      expect { |block| query.where(&block) }.to yield_control
+    end
+  end
+
+  describe '#with_criteria' do
+    let(:criteria) do
+      [
+        ['title',  :eq, 'The Caves of Steel'],
+        ['author', :eq, 'Isaac Asimov']
+      ]
+    end
+    let(:expected) { criteria }
+
+    it { expect(query).to respond_to(:with_criteria, true).with(1).argument }
+
+    it { expect(query.send(:with_criteria, criteria)).to be query }
+
+    it 'should append the criteria' do
+      expect { query.send(:with_criteria, criteria) }
+        .to change(query, :criteria)
+        .to be == expected
+    end
+
+    context 'when the query has criteria' do
+      let(:old_criteria) do
+        [
+          ['genre', :eg, 'Science Fiction']
+        ]
+      end
+      let(:expected) { old_criteria + criteria }
+
+      before(:example) do
+        query.send(:with_criteria, old_criteria)
+      end
+
+      it 'should append the criteria' do
+        expect { query.send(:with_criteria, criteria) }
+          .to change(query, :criteria)
+          .to be == expected
+      end
+    end
+  end
+end
