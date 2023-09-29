514cc18864bfe2ddf80e87b1630821d451831c54
Rob Smith <merlin@sleepingkingstudios.com>
2023-09-29 14:44:46 -0400

    Refactor Commands::Associations::FindMany.

--- spec/cuprum/collections/commands/associations/require_many_spec.rb
+++ spec/cuprum/collections/commands/associations/require_many_spec.rb
@@ -34,19 +34,122 @@ RSpec.describe Cuprum::Collections::Commands::Associations::RequireMany do
   end
 
   describe '#call' do
+    shared_examples 'should require the association for one entity' do
+      context 'when there is no matching entity' do
+        let(:expected_error) do
+          Cuprum::Collections::Errors::Associations::NotFound.new(
+            attribute_name:  association.query_key_name,
+            attribute_value: 0,
+            collection_name: association.name,
+            primary_key:     association.primary_key_query?
+          )
+        end
+
+        it 'should return a failing result' do
+          expect(command.call(**params))
+            .to be_a_failing_result
+            .with_error(expected_error)
+        end
+      end
+
+      context 'when there is one matching entity' do
+        let(:matching) { values[0..0] }
+
+        it 'should return a passing result with the matching results' do
+          expect(command.call(**params))
+            .to be_a_passing_result
+            .with_value(matching.first)
+        end
+      end
+    end
+
+    shared_examples 'should require the association for many entities' do
+      context 'when there are no matching entities' do
+        let(:expected_error) do
+          Cuprum::Collections::Errors::Associations::NotFound.new(
+            attribute_name:  association.query_key_name,
+            attribute_value: [0, 1],
+            collection_name: association.name,
+            primary_key:     association.primary_key_query?
+          )
+        end
+
+        it 'should return a failing result' do
+          expect(command.call(**params))
+            .to be_a_failing_result
+            .with_error(expected_error)
+        end
+      end
+
+      context 'when there are some matching entities' do
+        let(:matching) { values[0..0] }
+        let(:expected_error) do
+          Cuprum::Collections::Errors::Associations::NotFound.new(
+            attribute_name:  association.query_key_name,
+            attribute_value: [1],
+            collection_name: association.name,
+            primary_key:     association.primary_key_query?
+          )
+        end
+
+        it 'should return a failing result' do
+          expect(command.call(**params))
+            .to be_a_failing_result
+            .with_error(expected_error)
+        end
+      end
+
+      context 'when there are many matching entities' do
+        let(:matching) { values }
+
+        it 'should return a passing result with the matching results' do
+          expect(command.call(**params))
+            .to be_a_passing_result
+            .with_value(matching)
+        end
+      end
+    end
+
     let(:collection) do
       repository.find_or_create(
         name:           tools.str.pluralize(association.name),
         qualified_name: association.qualified_name
       )
     end
-    let(:matching) { [] }
     let(:non_matching) do
       {
         'id'   => 3,
         'name' => 'Kim Harrison'
       }
     end
+    let(:entities) do
+      [
+        { 'id' => 0, 'author_id' => 0, 'title' => 'Gideon the Ninth' },
+        { 'id' => 1, 'author_id' => 0, 'title' => 'Harrow the Ninth' },
+        {
+          'id'        => 2,
+          'author_id' => 1,
+          'title'     => 'The Word For World Is Forest'
+        }
+      ]
+    end
+    let(:values) do
+      [
+        {
+          'id'   => 0,
+          'name' => 'Tammsyn Muir'
+        },
+        {
+          'id'   => 1,
+          'name' => 'Ursula K. LeGuin'
+        }
+      ]
+    end
+    let(:inverse_key_name) do
+      association.with_inverse(resource).inverse_key_name
+    end
+    let(:params)   { {} }
+    let(:matching) { [] }
 
     def tools
       SleepingKingStudios::Tools::Toolbelt.instance
@@ -58,226 +161,112 @@ RSpec.describe Cuprum::Collections::Commands::Associations::RequireMany do
       collection.insert_one.call(entity: non_matching)
     end
 
-    it { expect(command).to be_callable.with_unlimited_arguments }
+    it 'should define the method' do
+      expect(command)
+        .to be_callable
+        .with(0).arguments
+        .and_keywords(:entities, :entity, :key, :keys)
+    end
 
     describe 'with no arguments' do
-      it 'should return a passing result' do
-        expect(command.call)
+      let(:error_message) do
+        'missing keyword :entity, :entities, :key, or :keys'
+      end
+
+      it 'should raise an exception' do
+        expect { command.call(**params) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with invalid keywords' do
+      let(:params) { { key: nil, custom: 'value', other: 'value' } }
+      let(:error_message) do
+        'invalid keywords :custom, :other'
+      end
+
+      it 'should raise an exception' do
+        expect { command.call(**params) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with ambiguous keywords' do
+      let(:params) { { key: nil, keys: [] } }
+      let(:error_message) do
+        'ambiguous keywords :key, :keys - must provide exactly one parameter'
+      end
+
+      it 'should raise an exception' do
+        expect { command.call(**params) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with entity: nil' do
+      let(:params) { { entity: nil } }
+
+      it 'should return a passing result with no results' do
+        expect(command.call(**params))
+          .to be_a_passing_result
+          .with_value(nil)
+      end
+    end
+
+    describe 'with entity: an Object' do
+      let(:params) { { entity: entities.first } }
+
+      include_examples 'should require the association for one entity'
+    end
+
+    describe 'with entities: an empty Array' do
+      let(:params) { { entities: [] } }
+
+      it 'should return a passing result with no results' do
+        expect(command.call(**params))
           .to be_a_passing_result
           .with_value([])
       end
     end
 
-    describe 'with one entity' do
-      let(:entities) do
-        [
-          { 'id' => 0, 'author_id' => 0, 'title' => 'Gideon the Ninth' }
-        ]
-      end
-      let(:expected_error) do
-        Cuprum::Collections::Errors::Associations::NotFound.new(
-          attribute_name:  association.query_key_name,
-          attribute_value: [0],
-          collection_name: association.name,
-          primary_key:     association.primary_key_query?
-        )
-      end
+    describe 'with entities: an Array of Objects' do
+      let(:params) { { entities: entities } }
 
-      it 'should return a failing result' do
-        expect(command.call(*entities))
-          .to be_a_failing_result
-          .with_error(expected_error)
-      end
+      include_examples 'should require the association for many entities'
+    end
 
-      context 'when there is one matching entity' do
-        let(:matching) do
-          [
-            {
-              'id'   => 0,
-              'name' => 'Tammsyn Muir'
-            }
-          ]
-        end
+    describe 'with key: nil' do
+      let(:params) { { key: nil } }
 
-        it 'should return a passing result with the matching results' do
-          expect(command.call(*entities))
-            .to be_a_passing_result
-            .with_value(matching)
-        end
+      it 'should return a passing result with no results' do
+        expect(command.call(**params))
+          .to be_a_passing_result
+          .with_value(nil)
       end
     end
 
-    describe 'with one key' do
-      let(:keys) { [0] }
-      let(:expected_error) do
-        Cuprum::Collections::Errors::Associations::NotFound.new(
-          attribute_name:  association.query_key_name,
-          attribute_value: [0],
-          collection_name: association.name,
-          primary_key:     association.primary_key_query?
-        )
-      end
+    describe 'with key: an Integer' do
+      let(:params) { { key: entities.first[inverse_key_name] } }
 
-      it 'should return a failing result' do
-        expect(command.call(*keys))
-          .to be_a_failing_result
-          .with_error(expected_error)
-      end
+      include_examples 'should require the association for one entity'
+    end
 
-      context 'when there is one matching entity' do
-        let(:matching) do
-          [
-            {
-              'id'   => 0,
-              'name' => 'Tammsyn Muir'
-            }
-          ]
-        end
+    describe 'with keys: an empty Array' do
+      let(:params) { { keys: [] } }
 
-        it 'should return a passing result with the matching results' do
-          expect(command.call(*keys))
-            .to be_a_passing_result
-            .with_value(matching)
-        end
+      it 'should return a passing result with no results' do
+        expect(command.call(**params))
+          .to be_a_passing_result
+          .with_value([])
       end
     end
 
-    describe 'with many entities' do
-      let(:entities) do
-        [
-          { 'id' => 0, 'author_id' => 0, 'title' => 'Gideon the Ninth' },
-          { 'id' => 1, 'author_id' => 0, 'title' => 'Harrow the Ninth' },
-          {
-            'id'        => 2,
-            'author_id' => 1,
-            'title'     => 'The Word For World Is Forest'
-          }
-        ]
-      end
-      let(:expected_error) do
-        Cuprum::Collections::Errors::Associations::NotFound.new(
-          attribute_name:  association.query_key_name,
-          attribute_value: [0, 1],
-          collection_name: association.name,
-          primary_key:     association.primary_key_query?
-        )
+    describe 'with keys: an Array of Integers' do
+      let(:params) do
+        { keys: entities.map { |entity| entity[inverse_key_name] } }
       end
 
-      it 'should return a failing result' do
-        expect(command.call(*entities))
-          .to be_a_failing_result
-          .with_error(expected_error)
-      end
-
-      context 'when there is one matching entity' do
-        let(:matching) do
-          [
-            {
-              'id'   => 0,
-              'name' => 'Tammsyn Muir'
-            }
-          ]
-        end
-        let(:expected_error) do
-          Cuprum::Collections::Errors::Associations::NotFound.new(
-            attribute_name:  association.query_key_name,
-            attribute_value: [1],
-            collection_name: association.name,
-            primary_key:     association.primary_key_query?
-          )
-        end
-
-        it 'should return a failing result' do
-          expect(command.call(*entities))
-            .to be_a_failing_result
-            .with_error(expected_error)
-        end
-      end
-
-      context 'when there are many matching entities' do
-        let(:matching) do
-          [
-            {
-              'id'   => 0,
-              'name' => 'Tammsyn Muir'
-            },
-            {
-              'id'   => 1,
-              'name' => 'Ursula K. LeGuin'
-            }
-          ]
-        end
-
-        it 'should return a passing result with the matching results' do
-          expect(command.call(*entities))
-            .to be_a_passing_result
-            .with_value(matching)
-        end
-      end
-    end
-
-    describe 'with many keys' do
-      let(:keys) { [0, 1] }
-      let(:expected_error) do
-        Cuprum::Collections::Errors::Associations::NotFound.new(
-          attribute_name:  association.query_key_name,
-          attribute_value: [0, 1],
-          collection_name: association.name,
-          primary_key:     association.primary_key_query?
-        )
-      end
-
-      it 'should return a failing result' do
-        expect(command.call(*keys))
-          .to be_a_failing_result
-          .with_error(expected_error)
-      end
-
-      context 'when there is one matching entity' do
-        let(:matching) do
-          [
-            {
-              'id'   => 0,
-              'name' => 'Tammsyn Muir'
-            }
-          ]
-        end
-        let(:expected_error) do
-          Cuprum::Collections::Errors::Associations::NotFound.new(
-            attribute_name:  association.query_key_name,
-            attribute_value: [1],
-            collection_name: association.name,
-            primary_key:     association.primary_key_query?
-          )
-        end
-
-        it 'should return a failing result' do
-          expect(command.call(*keys))
-            .to be_a_failing_result
-            .with_error(expected_error)
-        end
-      end
-
-      context 'when there are many matching entities' do
-        let(:matching) do
-          [
-            {
-              'id'   => 0,
-              'name' => 'Tammsyn Muir'
-            },
-            {
-              'id'   => 1,
-              'name' => 'Ursula K. LeGuin'
-            }
-          ]
-        end
-
-        it 'should return a passing result with the matching results' do
-          expect(command.call(*keys))
-            .to be_a_passing_result
-            .with_value(matching)
-        end
-      end
+      include_examples 'should require the association for many entities'
     end
 
     context 'when initialized with a singular resource' do
@@ -285,86 +274,16 @@ RSpec.describe Cuprum::Collections::Commands::Associations::RequireMany do
         Cuprum::Collections::Resource.new(name: 'book', singular: true)
       end
 
-      describe 'with no arguments' do
-        it 'should return a passing result' do
-          expect(command.call)
-            .to be_a_passing_result
-            .with_value(nil)
-        end
+      describe 'with entity: an Object' do
+        let(:params) { { entity: entities.first } }
+
+        include_examples 'should require the association for one entity'
       end
 
-      describe 'with one entity' do
-        let(:entities) do
-          [
-            { 'id' => 0, 'author_id' => 0, 'title' => 'Gideon the Ninth' }
-          ]
-        end
-        let(:expected_error) do
-          Cuprum::Collections::Errors::Associations::NotFound.new(
-            attribute_name:  association.query_key_name,
-            attribute_value: 0,
-            collection_name: association.name,
-            primary_key:     association.primary_key_query?
-          )
-        end
+      describe 'with key: an Integer' do
+        let(:params) { { key: entities.first[inverse_key_name] } }
 
-        it 'should return a failing result' do
-          expect(command.call(*entities))
-            .to be_a_failing_result
-            .with_error(expected_error)
-        end
-
-        context 'when there is one matching entity' do
-          let(:matching) do
-            [
-              {
-                'id'   => 0,
-                'name' => 'Tammsyn Muir'
-              }
-            ]
-          end
-
-          it 'should return a passing result with the matching results' do
-            expect(command.call(*entities))
-              .to be_a_passing_result
-              .with_value(matching.first)
-          end
-        end
-      end
-
-      describe 'with one key' do
-        let(:keys) { [0] }
-        let(:expected_error) do
-          Cuprum::Collections::Errors::Associations::NotFound.new(
-            attribute_name:  association.query_key_name,
-            attribute_value: 0,
-            collection_name: association.name,
-            primary_key:     association.primary_key_query?
-          )
-        end
-
-        it 'should return a failing result' do
-          expect(command.call(*keys))
-            .to be_a_failing_result
-            .with_error(expected_error)
-        end
-
-        context 'when there is one matching entity' do
-          let(:matching) do
-            [
-              {
-                'id'   => 0,
-                'name' => 'Tammsyn Muir'
-              }
-            ]
-          end
-
-          it 'should return a passing result with the matching results' do
-            expect(command.call(*keys))
-              .to be_a_passing_result
-              .with_value(matching.first)
-          end
-        end
+        include_examples 'should require the association for one entity'
       end
     end
   end
