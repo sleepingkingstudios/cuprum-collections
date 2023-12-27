7d8ff704a5a72605d646f84500c8d6ac35504281
Rob Smith <merlin@sleepingkingstudios.com>
2023-12-26 22:05:21 -0500

    Implement Basic::Scopes::NegationScope.

--- lib/cuprum/collections/basic/scopes.rb
+++ lib/cuprum/collections/basic/scopes.rb
@@ -9,5 +9,9 @@ module Cuprum::Collections::Basic
       'cuprum/collections/basic/scopes/conjunction_scope'
     autoload :CriteriaScope,
       'cuprum/collections/basic/scopes/criteria_scope'
+    autoload :DisjunctionScope,
+      'cuprum/collections/basic/scopes/disjunction_scope'
+    autoload :NegationScope,
+      'cuprum/collections/basic/scopes/negation_scope'
   end
 end
--- lib/cuprum/collections/rspec/contracts/scope_contracts.rb
+++ lib/cuprum/collections/rspec/contracts/scope_contracts.rb
@@ -485,6 +485,92 @@ module Cuprum::Collections::RSpec::Contracts
       end
     end
 
+    # Contract validating the behavior of a Negation scope implementation.
+    module ShouldFilterDataByLogicalNandContract
+      extend RSpec::SleepingKingStudios::Contract
+
+      # @!method apply(example_group)
+      #   Adds the contract to the example group.
+      #
+      #   @param example_group [RSpec::Core::ExampleGroup] the example group to
+      #     which the contract is applied.
+      contract do
+        shared_context 'with data' do
+          let(:data) do
+            Cuprum::Collections::RSpec::Fixtures::BOOKS_FIXTURES
+          end
+        end
+
+        context 'when the scope has no child scopes' do
+          let(:scopes) { [] }
+
+          describe 'with empty data' do
+            let(:data) { [] }
+
+            it { expect(filtered_data).to be == [] }
+          end
+
+          wrap_context 'with data' do
+            it { expect(filtered_data).to be == [] }
+          end
+        end
+
+        context 'when the scope has one child scope' do
+          let(:scopes) do
+            [
+              build_scope({ 'author' => 'J.R.R. Tolkien' })
+            ]
+          end
+
+          describe 'with empty data' do
+            let(:data)     { [] }
+            let(:expected) { data }
+
+            it { expect(filtered_data).to be == expected }
+          end
+
+          wrap_context 'with data' do
+            let(:expected) do
+              data.reject { |item| item['author'] == 'J.R.R. Tolkien' }
+            end
+
+            it { expect(filtered_data).to be == expected }
+          end
+        end
+
+        context 'when the scope has many child scopes' do
+          let(:scopes) do
+            [
+              build_scope({ 'author' => 'J.R.R. Tolkien' }),
+              build_scope({ 'series' => 'The Lord of the Rings' }),
+              build_scope do
+                { 'published_at' => less_than('1955-01-01') }
+              end
+            ]
+          end
+
+          describe 'with empty data' do
+            let(:data)     { [] }
+            let(:expected) { data }
+
+            it { expect(filtered_data).to be == expected }
+          end
+
+          wrap_context 'with data' do
+            let(:expected) do
+              data.reject do |item|
+                item['author'] == 'J.R.R. Tolkien' &&
+                  item['series'] == 'The Lord of the Rings' &&
+                  item['published_at'] < '1955-01-01'
+              end
+            end
+
+            it { expect(filtered_data).to be == expected }
+          end
+        end
+      end
+    end
+
     # Contract validating the behavior of a Conjunction scope implementation.
     module ShouldFilterDataByLogicalAndContract
       extend RSpec::SleepingKingStudios::Contract
