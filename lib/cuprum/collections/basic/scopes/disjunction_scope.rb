9d5e6d829286242a2d7de0e0afca656c59281fb8
Rob Smith <merlin@sleepingkingstudios.com>
2023-12-26 21:52:55 -0500

    Implement Basic::Scopes::DisjunctionScope.

--- lib/cuprum/collections/basic/scopes/disjunction_scope.rb
+++ lib/cuprum/collections/basic/scopes/disjunction_scope.rb
@@ -5,15 +5,15 @@ require 'cuprum/collections/basic/scopes'
 require 'cuprum/collections/scopes/container'
 
 module Cuprum::Collections::Basic::Scopes
-  # Scope for filtering data matching all of the given scopes.
-  class ConjunctionScope < Cuprum::Collections::Basic::Scope
+  # Scope for filtering data matching any of the given scopes.
+  class DisjunctionScope < Cuprum::Collections::Basic::Scope
     include Cuprum::Collections::Scopes::Container
 
-    # Returns true if the provided item matches all of the configured scopes.
+    # Returns true if the provided item matches any of the configured scopes.
     def match?(item:)
       super
 
-      scopes.all? { |scope| scope.match?(item: item) }
+      scopes.any? { |scope| scope.match?(item: item) }
     end
     alias matches? match?
   end
