5c4f9533c373032e3dd2ab6327d0ab0fd6a883ce
Rob Smith <sleepingkingstudios@gmail.com>
2024-08-25 16:49:27 -0400

    Update dependencies.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,8 @@ RSpec.configure do |config|
   config.extend  RSpec::SleepingKingStudios::Concerns::FocusExamples
   config.extend  RSpec::SleepingKingStudios::Concerns::IncludeContract
   config.extend  RSpec::SleepingKingStudios::Concerns::WrapExamples
+  config.include RSpec::SleepingKingStudios::Deferred::Consumer
+  config.include RSpec::SleepingKingStudios::Deferred::Provider
   config.include RSpec::SleepingKingStudios::Examples::PropertyExamples
   config.extend  Spec::WithContract
 
