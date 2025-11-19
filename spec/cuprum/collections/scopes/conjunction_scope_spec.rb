694701a2edef170957cb4fe1dc34c853d47c7f09
Rob Smith <sleepingkingstudios@gmail.com>
2025-11-18 19:03:50 -0500

    Define Scopes::ConjunctionExamples.

--- spec/cuprum/collections/scopes/conjunction_scope_spec.rb
+++ spec/cuprum/collections/scopes/conjunction_scope_spec.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
-require 'cuprum/collections/rspec/contracts/scopes/logical_contracts'
+require 'cuprum/collections/rspec/deferred/scopes/conjunction_examples'
 require 'cuprum/collections/scopes/conjunction_scope'
 require 'cuprum/collections/scopes/criteria_scope'
 
 RSpec.describe Cuprum::Collections::Scopes::ConjunctionScope do
-  include Cuprum::Collections::RSpec::Contracts::Scopes::LogicalContracts
+  include Cuprum::Collections::RSpec::Deferred::Scopes::ConjunctionExamples
 
   subject(:scope) { described_class.new(scopes:) }
 
   let(:scopes) { [] }
 
-  def build_scope(...)
-    Cuprum::Collections::Scope.new(...)
+  define_method :build_scope do |*args, **kwargs, &block|
+    Cuprum::Collections::Scope.new(*args, **kwargs, &block)
   end
 
-  include_contract 'should be a conjunction scope', abstract: true
+  include_deferred 'should implement the ConjunctionScope methods',
+    abstract: true
 end
