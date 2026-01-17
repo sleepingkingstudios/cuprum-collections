047c73122669562a1daab5150a6a1f2c97e6e294
Rob Smith <sleepingkingstudios@gmail.com>
2026-01-17 00:21:53 -0500

    Define Bronze namespace.

---
+++ spec/bronze_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'bronze'
+
+RSpec.describe Bronze do
+  describe '::VERSION' do
+    let(:version_pattern) do
+      /\A\d+\.\d+\.\d+(\.(alpha|beta|rc))?(\.\d+)?\z/
+    end
+
+    include_examples 'should define constant', :VERSION, -> { be_a String }
+
+    it { expect(described_class::VERSION).to match(version_pattern) }
+  end
+
+  describe '.gem_path' do
+    let(:expected) do
+      sep = File::SEPARATOR
+
+      __dir__.sub(/#{sep}spec#{sep}?\z/, '')
+    end
+
+    include_examples 'should define class reader',
+      :gem_path,
+      -> { be == expected }
+  end
+
+  describe '.version' do
+    include_examples 'should have class reader',
+      :version,
+      described_class::VERSION
+  end
+end
